303a3e46c57929615ad7b640364ee46fdfc38e3c
Derick Quartey <niikwartey1@gmail.com>
2016-09-22 15:31:43 -0400

    added board name validation

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,4 +2,6 @@ class Board < ApplicationRecord
   belongs_to :user, optional: true
   has_many :lists
   has_many :feeds
+
+  validates_length_of :name, maximum: 30, message: "board must be a maximium of 30 characters"
 end
