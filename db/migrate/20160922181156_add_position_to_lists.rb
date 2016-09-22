d8145e5b8ee2da766d39772be6d85284fc549c06
aehmt <calisahmet@gmail.com>
2016-09-22 14:28:19 -0400

    list position added

---
+++ db/migrate/20160922181156_add_position_to_lists.rb
@@ -0,0 +1,5 @@
+class AddPositionToLists < ActiveRecord::Migration[5.0]
+  def change
+    add_column :lists, :position, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922174828) do
+ActiveRecord::Schema.define(version: 20160922181156) do
 
   create_table "boards", force: :cascade do |t|
     t.string   "name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20160922174828) do
     t.datetime "updated_at", null: false
     t.integer  "board_id"
     t.datetime "duedate"
+    t.integer  "position"
   end
 
   create_table "users", force: :cascade do |t|
