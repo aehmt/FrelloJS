992230995f37ed5ddbd485062e01a19a2ef94c50
Derick Quartey <niikwartey1@gmail.com>
2016-09-22 11:54:41 -0400

    added somn

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,6 +2,7 @@ class ProfileController < ApplicationController
   def home
     @user = current_user
     @boards = @user.boards.order(:starred).reverse
+    @feeds = @boards.feeds
     @new_board = Board.new
   end
 end
