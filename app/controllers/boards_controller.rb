cbfaa0e2d74b8fa92612c1a834f6622e332cd376
Derick Quartey <niikwartey1@gmail.com>
2016-09-23 13:13:09 -0400

    added ui component

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,10 +1,10 @@
 require 'pry'
 class BoardsController < ApplicationController
-  before_action :set_board, only: [:show, :update, :destroy]
+  before_action :find_board, only: [:show, :update]
 
   def index
     @boards = current_user.boards
-    show_home
+    redirect_to root_path and return
   end
 
   def show
@@ -12,6 +12,7 @@ class BoardsController < ApplicationController
     @new_list = List.new(duedate: DateTime.now.utc.end_of_day)
     @new_card = Card.new
     @feeds = @board.feeds.reverse
+    # @feeds
   end
 
   def create
@@ -19,7 +20,7 @@ class BoardsController < ApplicationController
     if @board.save
       redirect_to user_boards_path(current_user)
     else
-      redirect_to root_path, flash[:alert] = "invalid something"
+      redirect_to root_path, alert: 'Invalid Something'
     end
   end
 
@@ -28,17 +29,12 @@ class BoardsController < ApplicationController
     redirect_to root_path
   end
 
-  def destroy
-    @board.destroy
-    redirect_to user_boards_path(current_user)
-  end
-
   private
     def board_params
       params.require(:board).permit(:name, :color, :private, :starred)
     end
 
-    def set_board
+    def find_board
       @board = Board.find_by(id: params[:id])
     end
 end
