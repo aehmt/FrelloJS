c82343de1acae76c7338cf870fd92970f1c0715d
aehmt <calisahmet@gmail.com>
2016-09-23 11:48:23 -0400

    class methods added to list model

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,12 @@ class List < ApplicationRecord
   belongs_to :board, optional: true
   has_many :cards
   has_many :feeds
+
+  def self.overdue
+    all.where("duedate < ?", Time.now)
+  end
+
+  def self.almost_overdue
+    all.where("duedate > ? and duedate < ?", 30.minutes.ago, Time.now)
+  end
 end
