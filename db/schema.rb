efb2235e53ed6f73a04983290f92f8fc14509cdf
aehmt <calisahmet@gmail.com>
2016-11-29 18:52:25 -0500

    atttemp3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922181156) do
+ActiveRecord::Schema.define(version: 20160926194116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,12 @@ ActiveRecord::Schema.define(version: 20160922181156) do
     t.integer  "position"
   end
 
+  create_table "shared_boards", force: :cascade do |t|
+    t.integer "collaborator_id"
+    t.integer "owner_id"
+    t.integer "board_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -75,6 +81,7 @@ ActiveRecord::Schema.define(version: 20160922181156) do
     t.integer  "role"
     t.string   "provider"
     t.string   "uid"
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["provider"], name: "index_users_on_provider", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
